c683c1a64e97cc23ff1994058da5630bb0d64768
Sujimichi <sujimichi@gmail.com>
2014-09-26 16:30:18 +0100

    changed name of ignore file to be more windows friendly. changed behaviour to coninue scanning if ther eis a unmatched entry, after prompting the user and waiting a few seconds

--- lib/KerbalX/ignore_file.rb
+++ lib/KerbalX/ignore_file.rb
@@ -3,21 +3,26 @@ module KerbalX
 
     def initialize path
 
-      ignore_file = File.join([path, "partmapper.ignore"])
+      ignore_file = File.join([path, "ignore_mods.txt"])
       if File.exists?(ignore_file)
         ignore_data = File.open(ignore_file, "r"){|f| f.readlines}.select{|line| 
           !line.match(/^#/)
-        }.map{|line| line.strip}.select{|line| !line.blank?}
+        }.map{|line| line.strip}.select{|line| !line.blank? && !line.include?("ExampleModtoSkip") }
         
         dirs = Dir.entries(File.join([path, "GameData"]))
         @ignore = ignore_data.select{|ig| dirs.include?(ig)}
         
+        
         if @ignore != ignore_data
-          msg = "\n\nAn entry in your partmapper.ignore does not match a folder in GameData"
+          puts "\n\n!!NOTE!!\nAn entry in your partmapper.ignore does not match a folder in GameData"
           no_match = ignore_data - @ignore
-          msg << "\n#{no_match.join (", ")} can't be found"
-          msg << "\nentires in partmapper.ignore need to EXACTLY match folders found in GameData"
-          raise msg #hmmmm, MSG
+          puts  "\n#{no_match.join (", ")} can't be found"         
+          puts  "\nentires in partmapper.ignore need to EXACTLY match folders in GameData"
+          
+          puts "\nIf you want to change the entry in partmapper.ignore then hit CTRL+C now"
+          puts "Otherwise I will carry on with the scan in 10 seconds."
+
+          sleep 10
         end
       end
     end
