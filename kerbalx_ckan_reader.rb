027de215eaa851b2be172510db6db6ba5618f353
Sujimichi <sujimichi@gmail.com>
2016-08-02 01:39:21 +0100

    comments

--- kerbalx_ckan_reader.rb
+++ kerbalx_ckan_reader.rb
@@ -1,9 +1,7 @@
 #This script runs the CKAN-meta reading actions
 #under construction, testing, not for general consumption, if swallowed seek medical attention.
 
-
 require 'KerbalX'
-
 
 puts "\nCKAN-Meta Reader for KerbalX.com - v#{KerbalX::VERSION}\n\n".green
 
@@ -11,7 +9,6 @@ puts "\nCKAN-Meta Reader for KerbalX.com - v#{KerbalX::VERSION}\n\n".green
 @site = "https://kerbalx.com"
 #@site = "http://localhost:3000"
 #@site = "http://kerbalx-stage.herokuapp.com"
-
 
 @path = Dir.getwd
 #@path = "/home/sujimichi/coding/lab/KerbalX-CKAN" 
@@ -25,7 +22,8 @@ KerbalX::Interface.new(@site, KerbalX::AuthToken.new(@path)) do |kerbalx|
   ckan_reader.process       #download new/updated mods and read part info
   ckan_reader.save_data     #write current mod data to file
 
-  reader_log = {  #get log info to be sent up to KerbalX server    
+  #get log info to be sent up to KerbalX server    
+  reader_log = {  
     :errors => ckan_reader.errors,
     :processed_mods => ckan_reader.processed_mods - ckan_reader.ignore_list,
     :reader_log => ckan_reader.message_log
@@ -35,6 +33,7 @@ KerbalX::Interface.new(@site, KerbalX::AuthToken.new(@path)) do |kerbalx|
   ckan_reader.pretty_json = false
   kerbalx.update_knowledge_base_with_ckan_data ckan_reader.json_data(:mod_data), reader_log.to_json
 
+  #poll the server to wait for the knowledgebase update to complete.
   kerbalx.after_knowledge_base_update do 
     #fetch list of parts without part data
     ckan_reader.msg "Fetching Parts without data from #{kerbalx.site}".blue
@@ -45,13 +44,11 @@ KerbalX::Interface.new(@site, KerbalX::AuthToken.new(@path)) do |kerbalx|
        
     #send part data back to site.
     kerbalx.update_knowledge_base_with_part_data part_data.parts.to_json
-        
   end
 
   unless ckan_reader.errors.empty?
     puts "errors:\n"
     ckan_reader.show_errors
   end
-
 
 end
